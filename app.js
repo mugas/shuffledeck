dba1082899da9d2bf98111bb78cf1cf891e7179f
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-11-13 23:59:48 +0200

    add diferent speeds for the shuffle

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ new Vue({
   el: "#app",
   data: {
     ranks: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
-    suits: ["♥", "♦", "♣", "♠"],
+    suits: ["&", "%", ")", "#"],
     cards: [],
     suitColor: {
-      "♥": "black",
-      "♦": "black",
-      "♣": "red",
-      "♠": "red"
-    }
+      "&": "black",
+      "%": "black",
+      ")": "red",
+      "#": "red"
+    },
+    shuffleSpeed: "shuffleMedium"
   },
   created() {
     this.displayInitialDeck();
@@ -19,12 +20,12 @@ new Vue({
       let id = 1;
       this.cards = [];
 
-      for (let i = 0; i < this.suits.length; i++) {
-        for (let j = 0; j < this.ranks.length; j++) {
+      for (let s = 0; s < this.suits.length; s++) {
+        for (let r = 0; r < this.ranks.length; r++) {
           let card = {
             id: id,
-            rank: this.ranks[j],
-            suits: this.suits[i]
+            rank: this.ranks[r],
+            suit: this.suits[s]
           };
           this.cards.push(card);
           id++;
@@ -32,12 +33,12 @@ new Vue({
       }
     },
     shuffleDeck() {
-      for (let m = this.cards.length - 1; m > 0; m--) {
-        let randomIndex = Math.floor(Math.random() * m);
+      for (let i = this.cards.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * i);
 
-        let temp = this.card[m];
+        let temp = this.cards[i];
 
-        Vue.set(this.cards, m, this.cards[randomIndex]);
+        Vue.set(this.cards, i, this.cards[randomIndex]);
         Vue.set(this.cards, randomIndex, temp);
       }
     }
